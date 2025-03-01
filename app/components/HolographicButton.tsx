ad401d29a1e0fc36efd1b253689a810097cf8cd3
iliamahns <43922964+iliamahns@users.noreply.github.com>
2025-03-01 11:59:29 +0100

    icon

--- app/components/HolographicButton.tsx
+++ app/components/HolographicButton.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 interface HolographicButtonProps {
   href: string;
@@ -9,12 +10,14 @@ interface HolographicButtonProps {
 }
 
 export default function HolographicButton({ href, text, imagePath, size = 'lg' }: HolographicButtonProps) {
+  const pathname = usePathname();
+  const isCurrentPage = pathname === href;
   const dimensions = size === 'lg' ? 'w-40 h-40' : 'w-14 h-14';
   const textSize = size === 'lg' ? 'text-2xl' : 'text-xs';
   
   return (
     <Link href={href} className="group relative">
-      <div className={`${dimensions} rounded-full overflow-hidden cursor-pointer hover:scale-105 hover:shadow-lg hover:opacity-80 ease-linear transition-transform relative`}>
+      <div className={`${dimensions} rounded-full overflow-hidden cursor-pointer hover:scale-105 hover:shadow-lg hover:opacity-80 ease-linear transition-transform relative ${isCurrentPage ? 'opacity-60' : ''}`}>
         <div className="absolute inset-0 rounded-full overflow-hidden">
           <Image
             src={imagePath}
